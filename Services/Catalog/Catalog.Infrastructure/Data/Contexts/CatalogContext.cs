af5e9810f8ee0aad4a054796ecdacb63e34f5705
saadpy11 <saadahmad@gmail.com>
2026-04-28 06:59:42 +0300

    feat(Catalog)  : add productRepo Implementation

---
+++ Services/Catalog/Catalog.Infrastructure/Data/Contexts/CatalogContext.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Infrastructure.Data.Contexts;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data.Contexts
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products    {get;}
+        public IMongoCollection<ProductBrand> Brands {get;}
+        public IMongoCollection<ProductType> Types { get; }
+
+
+
+        public  CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Brands = database.GetCollection<ProductBrand>(configuration["DatabaseSettings:BrandsCollection"]);
+            Types = database.GetCollection<ProductType>(configuration["DatabaseSettings:TypesCollection"]);
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:ProductsCollection"]);
+
+            _ = BrandContextSeed.SeedBrands(Brands);
+            _ = TypeContextSeed.SeedTypes(Types);
+            _ = ProductContextSeed.SeedDataAsync(Products);
+        }
+
+    }
+}
---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data.Contexts;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypeRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+             _context = context;
+        }
+        public async Task<Product> CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var deletedProduct = await _context.Products.DeleteOneAsync(p => p.Id == id);
+            return deletedProduct.IsAcknowledged && deletedProduct.DeletedCount > 0;
+        }
+
+        public async Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            return await _context.Brands.Find(_ => true).ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _context.Products.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsByBrand(string name)
+        {
+            return await _context.Products.Find(p => p.Brand.BrandName.ToLower().Contains(name.ToLower())).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsByName(string name)
+        {
+            return await _context.Products.Find(p => p.Name.ToLower().Contains(name.ToLower())).ToListAsync();
+        }
+
+        public async Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            return await _context.Types.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updatedProduct = await _context.Products.ReplaceOneAsync(p => p.Id == product.Id, product);
+            return updatedProduct.IsAcknowledged && updatedProduct.ModifiedCount > 0;
+        }
+
+
+    }
+}
